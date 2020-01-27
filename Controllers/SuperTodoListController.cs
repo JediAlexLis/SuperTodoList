0fc6b85d646551eaf2525723f8a543cd0a6ab38a
JediAlexLis <alisitzin@gmail.com>
2020-01-27 17:12:14 +0300

    Added the main controller

---
+++ Controllers/SuperTodoListController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SuperTodoList.Controllers
+{
+    public class SuperTodoListController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
