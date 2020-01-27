2bcd1ac52482ae33dcf99c4e56b26b8ac15bd2cb
JediAlexLis <alisitzin@gmail.com>
2020-01-27 17:16:38 +0300

    Added an 'Index' view

--- Controllers/SuperTodoListController.cs
+++ Controllers/SuperTodoListController.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SuperTodoList.Models;
 
 namespace SuperTodoList.Controllers
 {
     public class SuperTodoListController : Controller
     {
-        public IActionResult Index()
+        private readonly SuperTodoListContext _context;
+
+        public SuperTodoListController(SuperTodoListContext context)
         {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+
             return View();
         }
     }
