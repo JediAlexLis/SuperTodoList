9aa4195f52082a9d2d4f5bc03a2a186ceb1e9938
JediAlexLis <alisitzin@gmail.com>
2020-01-27 17:00:21 +0300

    Configuring connection to an SQL Server

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using SuperTodoList.Models;
 
 namespace SuperTodoList
 {
@@ -24,6 +26,9 @@ namespace SuperTodoList
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<SuperTodoListContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("SuperTodoList")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
